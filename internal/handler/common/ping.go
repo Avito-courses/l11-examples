ecb7e8426c100aea6633f78a493a87b0e189fe94
aychirkin <aychirkin@avito.ru>
2026-01-16 01:15:31 +0300

    Revert "edit linters config"
    This reverts commit 6fa9a335c533a3d630ec41e798f4a8f357702ada.

--- internal/handler/common/ping.go
+++ internal/handler/common/ping.go
@@ -7,6 +7,5 @@ import (
 
 func Ping(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
 	_ = json.NewEncoder(w).Encode(map[string]string{"message": "pong"})
 }
